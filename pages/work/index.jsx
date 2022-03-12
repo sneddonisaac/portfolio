13174270b0bc7750038c47c36931ee79daeb4928
Isaac Sneddon-Jenkins <info.isaacsneddon@gmail.com>
2022-03-12 14:53:44 +0000

    initial develop branch commit

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import Layout from '../../templates/Layout'
 import WorkHero from '../../components/organisms/Heros/WorkHero'
 import WorksGrid from '../../components/organisms/WorksGrid.js/WorksGrid'
+import CustomCursor from "../../components/atoms/CustomCursor/CustomCursor";
 
 export default function Works() {
   const [works, setWorks] = useState([])
