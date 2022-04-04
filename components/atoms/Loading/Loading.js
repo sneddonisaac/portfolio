f7da0b52b7dc6e3b4c09dc5936bbf213d06e03ba
Isaac Sneddon-Jenkins <info.isaacsneddon@gmail.com>
2022-04-04 19:55:03 +0000

    feat: Loading bar

--- components/atoms/Loading/Loading.js
+++ components/atoms/Loading/Loading.js
@@ -1,19 +1,12 @@
-/* export default function Loading({ value }) {
-    return (
-        <div className="z-100 flex h-screen w-full items-center justify-center bg-white px-12">
-            <progress className="w-full bg-black text-black" max='100' value={value} />
-        </div>
-    )
-} */
-
 import * as Progress from '@radix-ui/react-progress';
 
 export default function Loading({ value }) {
     return (
         <div className="z-100 flex h-screen w-full items-center justify-center bg-white px-12">
             <Progress.Root value={value} className="w-full">
-                <Progress.Indicator style={{ width: `${value}%` }} className="w-full bg-black" max={100} />
+                <Progress.Indicator style={{ width: `${value}%` }} className="w-full h-4 bg-black" max={110} />
             </Progress.Root>
+			<span className="text-5xl">{value}</span>
         </div>
     )
 }
