732cd26da4f878cb14f76fc0f3bbc2caf6a665e2
Isaac Sneddon-Jenkins <info.isaacsneddon@gmail.com>
2022-04-04 19:40:06 +0000

    style: thinner loading bar

--- components/atoms/Loading/Loading.js
+++ components/atoms/Loading/Loading.js
@@ -11,7 +11,7 @@ export default function Loading({ value }) {
     return (
         <div className="z-100 flex h-screen w-full items-center justify-center bg-white px-12">
             <Progress.Root value={value} className="w-full">
-                <Progress.Indicator style={{ width: `${value}%` }} className="w-full h-4 bg-black" max={100} />
+                <Progress.Indicator style={{ width: `${value}%` }} className="w-full h-4 bg-black" max={110} />
             </Progress.Root>
 			<span className="text-5xl">{value}</span>
         </div>
