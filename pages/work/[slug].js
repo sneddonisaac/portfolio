dc395c47629ca5fd63c8b3cdefe4b13a0e7a6f6b
Isaac Sneddon-Jenkins <hello@isaacsneddonjenkins.co.uk>
2022-07-20 01:14:24 +0200

    visit site button fixed

--- pages/work/[slug].js
+++ pages/work/[slug].js
@@ -2,15 +2,36 @@ import Layout from '../../templates/Layout'
 import { allWorkPages, getWorkBySlug } from "../../lib/API";
 import Components from '../../utils/page-sections/individual-work';
 import ProjectHero from '../../components/organisms/Heros/ProjectHero';
+import Link from 'next/link';
 
 export default function Works({ work }) {
     return (
         <Layout>
             <ProjectHero data={work} />
-            {work?.content?.map((component, index) => (
-                <Components data={component} key={index} />
-            ))}
+            {work.content ? (
+                work.content?.map((component, index) => (
+                    <Components data={component} key={index} />
+                ))
+            ) : (
+                <div className='w-full flex justify-center items-center'>
+                    <h1>Sorry there doesn't see be any notes on this project yet</h1>
+                </div>
+            )}
+            <ProjectButtons data={work.projectLink} />
         </Layout>
+    )
+}
+
+function ProjectButtons({ data }) {
+    console.log("🚀 ~ file: [slug].js ~ line 25 ~ ProjectButtons ~ data", data)
+    return (
+        <div className='fixed right-8 bottom-16'>
+            <Link href={data}>
+                <a className='py-6 px-14 rounded-full border-2 border-black bg-black text-white hover:bg-white hover:text-black hover:scale-105 transition-all duration-300 ease-in-out'>
+                    Visit Site
+                </a>
+            </Link>
+        </div>
     )
 }
 
