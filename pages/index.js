f431745c8e37ea794df9cf3689e3bedf83d440cf
Isaac Sneddon-Jenkins <hello@isaacsneddonjenkins.co.uk>
2022-07-20 13:46:51 +0200

    fix: fetch request homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 // Data
-import {  getLandingPageQuery } from '../lib/queries';
-import { client } from '../utils/hygraph-client';
-import { getFeaturedWork } from '../lib/API';
+import { getFeaturedWork, getLandingPage } from '../lib/API';
 
 // UI
 import Layout from '../templates/Layout';
@@ -9,7 +7,7 @@ import LandingHero from '../components/organisms/Heros/LandingHero';
 import Components from '../utils/page-sections/landing-page';
 
 export async function getStaticProps() {
-  const { page } = await client.request(getLandingPageQuery)
+  const page = await getLandingPage()
   const works = await getFeaturedWork()
   return {
     props: {
@@ -20,10 +18,11 @@ export async function getStaticProps() {
 }
 
 export default function Home({page, works}) {
+  console.log("🚀 ~ file: index.js ~ line 21 ~ Home ~ page", page)
   return (
     <Layout title={page.title}>
       <LandingHero data={works[0]} />
-      {page.content.map(( component, index) => (
+      {page?.content?.map(( component, index) => (
         <Components data={component} key={index} />
       ))}
     </Layout>
