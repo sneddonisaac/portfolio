9023e45a90e14d6a345ae50409e9c0486043f0cc
Isaac Sneddon-Jenkins <hello@isaacsneddonjenkins.co.uk>
2022-08-17 23:30:29 +0100

    fix: noise bg

--- components/organisms/Headers/NoisyGradient5050Full.js
+++ components/organisms/Headers/NoisyGradient5050Full.js
@@ -8,10 +8,10 @@ export default function NoisyGradient5050Full(props) {
             <div className='gradient-bg w-full h-full'>
                 <div className='noise-bg absolute inset-0'/>
                 <Container>
-                    <div className='noise-bg absolute inset-0 z-0'/>
+                    <div className='noise-bg absolute inset-0 z-[-1]'/>
                     <div
-                        className='z-10 grid place-content-center auto-rows-[_0.5fr] sm:grid-cols-3 gap-16 py-12 px-16 text-white h-fit text-center sm:text-left'>
-                        <h1 className={clsx('text-white text-6xl h-fit font-satoshi-bold col-span-2', !props.paragraph && 'text-center col-span-3')}>
+                        className='text-white z-10 grid place-content-center auto-rows-[_0.5fr] sm:grid-cols-3 gap-16 py-12 px-16 text-white h-fit text-center sm:text-left'>
+                        <h1 className={clsx('text-6xl h-fit font-satoshi-bold col-span-2', !props.paragraph && 'text-center col-span-3')}>
                             {props.NoisyFullHeading}
                         </h1>
                         {props.paragraph && (
