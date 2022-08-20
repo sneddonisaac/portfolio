64e885f41b891da63332d29be8121bb615f160a4
Isaac Sneddon-Jenkins <hello@isaacsneddonjenkins.co.uk>
2022-08-20 21:58:20 +0100

    fix: initial value in localStorage fixes for ThemeSelector.js

--- components/atoms/ThemeSelector.js
+++ components/atoms/ThemeSelector.js
@@ -1,15 +1,20 @@
 import { useTheme } from 'next-themes'
 import { useLoaded } from '@/utils/hooks/useLoaded'
 import clsx from 'clsx'
+import { useEffect } from 'react'
 
 export default function ThemeSelector() {
     const { theme, setTheme } = useTheme()
     const loaded = useLoaded()
 
+    useEffect(() => {
+        setTheme(localStorage.theme)
+    }, [])
+
     return (
         <div
             className={
-                'z-100 fixed bottom-8 right-8 h-fit w-fit rounded-md bg-black py-2 px-3 dark:bg-white'
+                'z-100 fixed bottom-8 right-8 h-fit w-fit rounded-md bg-black py-2 px-3 shadow-2xl dark:bg-white'
             }
         >
             <button
