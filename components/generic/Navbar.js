0485cd80a58f852227d83b05065b70381e3b4c48
Isaac Sneddon-Jenkins <hello@isaacsneddonjenkins.co.uk>
2022-10-29 19:49:34 +0100

    style: moving both theme selector and menu bar inline for further UX compatability

--- components/generic/Navbar.js
+++ components/generic/Navbar.js
@@ -10,29 +10,35 @@ const nav = [
 ]
 
 export default function Navbar() {
-    const router = useRouter()
     return (
         <div
             id="Navbar"
-            className="z-[130] h-fit w-full bg-transparent font-satoshi-regular"
+            className="fixed inset-x-0 bottom-5 z-[130] mx-auto flex h-fit w-full justify-around bg-transparent font-satoshi-regular sm:bottom-16 sm:justify-between sm:px-16"
         >
-            <div className="fixed inset-x-0 bottom-16 z-[130] mx-auto flex w-2/3 items-center justify-between rounded-md bg-gray-300 py-3 px-5 text-sm shadow-lg dark:bg-gray-50 dark:text-black sm:w-1/3">
-                {nav.map((item, index) => (
-                    <Link key={index} href={item.path}>
-                        <a
-                            className={`cursor-pointer uppercase decoration-2 underline-offset-4 ${
-                                `/${router.pathname.split('/')[1]}` ===
-                                item.path
-                                    ? 'underline'
-                                    : ''
-                            }`}
-                        >
-                            {item.title}
-                        </a>
-                    </Link>
-                ))}
-            </div>
+            <div className="hidden w-11 sm:flex" />
+            <MenuBar />
             <ThemeSelector />
+        </div>
+    )
+}
+
+function MenuBar() {
+    const router = useRouter()
+    return (
+        <div className="z-[130] flex h-11 w-2/3 items-center justify-between rounded-md bg-gray-300 py-3 px-5 text-sm shadow-lg dark:bg-gray-50 dark:text-black sm:w-1/3">
+            {nav.map((item, index) => (
+                <Link key={index} href={item.path}>
+                    <a
+                        className={`cursor-pointer uppercase decoration-2 underline-offset-4 ${
+                            `/${router.pathname.split('/')[1]}` === item.path
+                                ? 'underline'
+                                : ''
+                        }`}
+                    >
+                        {item.title}
+                    </a>
+                </Link>
+            ))}
         </div>
     )
 }
