e4e30fa2875c07521003e6fd643c61c03969e137
Isaac Sneddon-Jenkins <hello@isaacsneddonjenkins.co.uk>
2022-11-05 06:14:01 +0000

    fix: 404 page on mobile not working

--- pages/404.js
+++ pages/404.js
@@ -8,13 +8,13 @@ export default function ErrorPage() {
                 <div id="scroll">
                     <div id="scroll-title">
                         <div>
-                            <h1>44&nbsp;</h1>
                             <h1>404&nbsp;</h1>
                             <h1>404&nbsp;</h1>
                             <h1>404&nbsp;</h1>
                             <h1>404&nbsp;</h1>
                             <h1>404&nbsp;</h1>
-                            <h1>44&nbsp;</h1>
+                            <h1>404&nbsp;</h1>
+                            <h1>404&nbsp;</h1>
                             <h1>404&nbsp;</h1>
                             <h1>404&nbsp;</h1>
                             <h1>404&nbsp;</h1>
@@ -22,21 +22,21 @@ export default function ErrorPage() {
                             <h1>404&nbsp;</h1>
                             <h1>404&nbsp;</h1>
                         </div>
-                        <p className="w-[50vw] text-center text-3xl lg:text-xl">
-                            It looks like you are lost,{' '}
-                            <Link href="/">
-                                <a
-                                    id="interactable"
-                                    data-type="home"
-                                    className="decoration-3 underline underline-offset-4 transition-all duration-300 ease-in-out hover:text-orange-600 hover:decoration-orange-600"
-                                >
-                                    let&apos;s get you back.
-                                </a>
-                            </Link>
-                        </p>
                     </div>
                 </div>
             </div>
+            <p className="-mt-16 text-center text-3xl">
+                It looks like you are lost,{' '}
+                <Link href="/">
+                    <a
+                        id="interactable"
+                        data-type="home"
+                        className="decoration-3 underline underline-offset-4 transition-all duration-300 ease-in-out hover:text-orange-600 hover:decoration-orange-600"
+                    >
+                        let&apos;s get you back.
+                    </a>
+                </Link>
+            </p>
         </BasicLayout>
     )
 }
