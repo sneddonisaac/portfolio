10f1bf7a6af1dbf94488adc283bdc513abd174de
sneddonisaac <hello@isaacsneddonjenkins.co.uk>
2022-12-23 03:43:58 +0000

    push

--- README.md
+++ README.md
@@ -4,7 +4,5 @@ Welcome to my portfolio! I built this using:
 
 - Nextjs
 - TailwindCSS
-- Sanity
-- Framer Motion
 
 Please feel free to go through my code and find any mistakes or fork and create your own version :)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,6 @@ import Script from 'next/script'
 import { ThemeProvider } from 'next-themes'
 import { DefaultSeo } from 'next-seo'
 import { SEO } from 'seo.config'
-import { DevSupport } from '@react-buddy/ide-toolbox'
-import { ComponentPreviews, useInitial } from '../dev'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
@@ -44,12 +42,6 @@ function MyApp({ Component, pageProps }: AppProps) {
             </Script>
             {/* default seo settings */}
             <DefaultSeo {...SEO} />
-            <DevSupport
-                ComponentPreviews={ComponentPreviews}
-                useInitialHook={useInitial}
-            >
-                <Component {...pageProps} />
-            </DevSupport>
         </ThemeProvider>
     )
 }
